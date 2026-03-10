5736fb096f72c6514555c0ed91591f91db0e504d
Timur Gorashchenko <timyr98239718@mail.ru>
2026-03-11 00:44:09 +0300

    done

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import UserViewSet, PaymentListView
 
 router = DefaultRouter()
 router.register(r'', UserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('payments/', PaymentListView.as_view(), name='payment-list'),
 ]

f20b5e1267f203dd953828f4a44b96ddb3bd8642
Timur Gorashchenko <timyr98239718@mail.ru>
2026-03-17 00:10:26 +0300

    done

--- materials/models.py
+++ materials/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-
+from django.conf import settings
 
 class Course(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название')
     preview = models.ImageField(upload_to='courses/previews/', verbose_name='Превью', blank=True, null=True)
     description = models.TextField(verbose_name='Описание', blank=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                              related_name='courses', verbose_name='Владелец', null=True, blank=True)
 
     class Meta:
         verbose_name = 'Курс'
@@ -20,7 +22,8 @@ class Lesson(models.Model):
     preview = models.ImageField(upload_to='lessons/previews/', verbose_name='Превью', blank=True, null=True)
     video_link = models.URLField(verbose_name='Ссылка на видео', blank=True)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons', verbose_name='Курс')
-
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                              related_name='lessons', verbose_name='Владелец', null=True, blank=True)
     class Meta:
         verbose_name = 'Урок'
         verbose_name_plural = 'Уроки'
